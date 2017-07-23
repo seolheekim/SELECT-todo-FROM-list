e609c48e7d21ab8b33599aa2e641fe26d51fb5a7
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-22 16:42:02 -1000

    fixed -- comments in .sql

--- todo_app.sql
+++ todo_app.sql
@@ -18,12 +18,12 @@
 
   \c todo_app
 
--- #6 - #11 Write a query to create a table named tasks using the Initial columns detailed below
---     -- Define column id as the table's primary key
---     -- Write queries to accomplish the following
---       -- remove the column named completed
---       -- add a column to tasks named completed_at:timestamp, that may be NULL, and has a default value of NULL.
---       -- change the updated_at column to not allow NULL values, and have a default value of now()
+-- #6 #11 Write a query to create a table named tasks using the Initial columns detailed below
+    -- Define column id as the table's primary key
+    -- Write queries to accomplish the following
+    -- remove the column named completed
+    -- add a column to tasks named completed_at:timestamp, that may be NULL, and has a default value of NULL.
+    -- change the updated_at column to not allow NULL values, and have a default value of now()
   DROP TABLE IF EXISTS tasks;
 
   CREATE TABLE tasks (
@@ -42,18 +42,18 @@
   ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT NOW();
 
 -- #12 create a new task, by only setting values (not defining which columns) id = default value
--- -- title = 'Study SQL'
--- -- description = 'Complete this exercise'
--- -- created_at = now()
--- -- updated_at = now()
--- -- completed_at = NULL
+-- title = 'Study SQL'
+-- description = 'Complete this exercise'
+-- created_at = now()
+-- updated_at = now()
+-- completed_at = NULL
 
 INSERT INTO tasks
 VALUES (DEFAULT, 'Study SQL', 'Complete this exercise', NOW(), NOW(), NULL);
 
 -- #13 create a new task
--- -- title = 'Study PostgreSQL'
--- -- description = 'Read all the documentation'
+-- title = 'Study PostgreSQL'
+-- description = 'Read all the documentation'
 
 INSERT INTO tasks (title, description)
 VALUES ( 'Study PostgreSQL', 'Read all the documentation' );
@@ -75,22 +75,22 @@ SELECT title, description FROM tasks WHERE completed_at = NULL;
 SELECT * FROM tasks ORDER BY created_at DESC;
 
 -- #18 create a new task
--- -- title = 'mistake 1'
--- -- description = 'a test entry'
+-- title = 'mistake 1'
+-- description = 'a test entry'
 
 INSERT INTO tasks (title, description)
 VALUES( 'mistake 1', 'a test entry');
 
 -- #19 create a new task
--- -- title = 'mistake 2'
--- -- description = 'another test entry'
+-- title = 'mistake 2'
+-- description = 'another test entry'
 
 INSERT INTO tasks (title, description)
 VALUES('mistake 2', 'another test entry');
 
 -- #20 create a new task
--- -- title = 'third mistake'
--- -- description = 'another test entry'
+-- title = 'third mistake'
+-- description = 'another test entry'
 
 INSERT INTO tasks (title, description)
 VALUES('third mistake', 'another test entry');
